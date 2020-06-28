225c06e334e59e2007d96f05865694f09b38e0c0
Pablo Gutiérrez Barceló <pablo@gutierrezbarcelo.com>
2020-06-28 12:13:59 +0200

    Fixed connector in sendQuery

--- app_refactor.py
+++ app_refactor.py
@@ -8,11 +8,7 @@
 import requests
 import mysql.connector
 import datetime
-import asyncio
-import datetime
 from mysql.connector import errorcode
-from requests.auth import HTTPBasicAuth
-import xml.etree.ElementTree as ET
 import numpy as np
 ##### DATA PRODUCTION #####
 protocol = 'https://'
@@ -29,7 +25,7 @@ oldCategorie = "164"  # INSERT NUMBER OF OLD CATEGORIE
 newCategorie = "112"  # INSERT NUMBER OF NEW CATEGORIE
 
 
-def sendQuery(query, fetch=None, dictionaryMode=False):
+def sendQuery(query, cnx, fetch=None, dictionaryMode=False):
     cursor = cnx.cursor(dictionary=dictionaryMode)
     cursor.execute(query)
     print(query)
@@ -52,38 +48,36 @@ if __name__ == "__main__":
         go = input('Are you sure? You will move your products from category {0} => to => {1}? (sure/n): '.format(
             oldCategorie, newCategorie))
         if go.upper() == 'SURE':
+            cnx = mysql.connector.connect(**config)
             fromTime = datetime.datetime.now()
 
-            # Create connection
-            cnx = mysql.connector.connect(**config)
             results = sendQuery("SELECT id_product FROM ps_product WHERE id_category_default={0}".format(
-                oldCategorie), "all", True)
-            cursor = cnx.cursor(dictionary=True)
+                oldCategorie), cnx, "all", True)
             print(results)
             # Get all product with default_category in oldCategorie
 
             for idorder in results:
                 sendQuery("UPDATE ps_product SET id_category_default={0} WHERE id_product={1}".format(
-                    newCategorie, idorder['id_product']))
+                    newCategorie, idorder['id_product']), cnx)
 
                 sendQuery("UPDATE ps_product_shop SET id_category_default={0} WHERE id_product={1}".format(
-                    newCategorie, idorder['id_product']))
+                    newCategorie, idorder['id_product']), cnx)
 
                 # Remove that product in that categorie ps_category_product
                 sendQuery("DELETE FROM ps_category_product WHERE id_product={0} AND id_category={1}".format(idorder['id_product'],
-                                                                                                            oldCategorie))
+                                                                                                            oldCategorie), cnx)
                 # Check if exist product in ps_category_product
                 result = sendQuery("SELECT id_product FROM `ps_category_product` WHERE id_category = {0} ORDER BY position DESC LIMIT 1".format(
-                    newCategorie), 'oneResult')[0]
+                    newCategorie), cnx, 'oneResult')[0]
                 print(result)
                 if result == None:
                     # Get last position in in ps_category_product on new categorie
                     nextPosition = int(sendQuery("SELECT position FROM `ps_category_product` WHERE id_category = {0} ORDER BY position DESC LIMIT 1".format(
-                        newCategorie), "oneResult")[0]) + 1
+                        newCategorie), cnx, "oneResult")[0]) + 1
 
                     # Add new position in new categorie ps_category_product
                     sendQuery("INSERT INTO ps_category_product VALUES ({0},{1},{2})".format(
-                        newCategorie, idorder['id_product'], nextPosition))
+                        newCategorie, idorder['id_product'], nextPosition), cnx)
 
                 else:
                     print("Exist position, skip")
@@ -98,35 +92,31 @@ if __name__ == "__main__":
             # Now check products without default categorie but with secondary
 
             # Get all products with oldCategorie
-            cursor = cnx.cursor(dictionary=True)
-            query = "SELECT id_product FROM ps_category_product WHERE id_category = {0}".format(
-                oldCategorie)
-            cursor.execute(query)
-            results = cursor.fetchall()
+            results = sendQuery("SELECT id_product FROM ps_category_product WHERE id_category = {0}".format(
+                oldCategorie), cnx, "all", True)
+
             print("Products loaded: {0}".format(len(results)))
-            cursor.close()
             for idorder in results:
                 # Check if exist product in ps_category_product (new categorie):
                 result = sendQuery("SELECT id_product FROM `ps_category_product` WHERE id_category = {0} AND id_product={1} ORDER BY position DESC LIMIT 1".format(
-                    newCategorie, idorder['id_product']), "oneResult")
+                    newCategorie, idorder['id_product']), cnx, "oneResult")
 
                 print("\nChecking if exist in new Categorie...")
                 print(result)
-                cursor.close()
                 if result == None:
                     # Get last position in ps_category_product on new categorie
                     nextPosition = int(sendQuery("SELECT position FROM `ps_category_product` WHERE id_category = {0} ORDER BY position DESC LIMIT 1".format(
-                        newCategorie), "oneResult")[0]) + 1
+                        newCategorie), cnx, "oneResult")[0]) + 1
 
                     # Add new position in new categorie ps_category_product
                     sendQuery("INSERT INTO ps_category_product VALUES ({0},{1},{2})".format(
-                        newCategorie, idorder['id_product'], nextPosition))
+                        newCategorie, idorder['id_product'], nextPosition), cnx)
 
                 else:
                     print("Exist position, skip")
                 # Remove that product in old categorie ps_category_product
                 sendQuery("DELETE FROM ps_category_product WHERE id_product={0} AND id_category={1}".format(idorder['id_product'],
-                                                                                                            oldCategorie))
+                                                                                                            oldCategorie), cnx)
 
             cnx.close()
         else:
